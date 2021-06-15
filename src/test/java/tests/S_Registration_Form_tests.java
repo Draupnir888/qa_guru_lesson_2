1f2aa71fbe8c2be82440738fa76a42386e8f6038
Михаил <balduin888@gmail.com>
2021-06-15 17:12:52 +0500

    Не работает проверка

--- src/test/java/tests/S_Registration_Form_tests.java
+++ src/test/java/tests/S_Registration_Form_tests.java
@@ -1,9 +1,13 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+
+import javax.swing.text.html.parser.DTD;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -12,7 +16,9 @@ import static com.codeborne.selenide.Selenide.*;
 public class S_Registration_Form_tests {
     @BeforeAll
     static void setup() {
-        Configuration.startMaximized = true;
+        Configuration.browserSize = "2500x1500";
+        //Configuration.startMaximized = true;
+        Configuration.headless = true;
     }
 
     @Test
@@ -22,7 +28,6 @@ public class S_Registration_Form_tests {
 
 
         open("https://demoqa.com/automation-practice-form");
-
 
         $("[id=firstName]").setValue("somefirstName");
         $("[id=lastName]").setValue("somelastName");
@@ -38,28 +43,37 @@ public class S_Registration_Form_tests {
         $(byText("1989")).click();
         $(byText("18")).click();
 
+        //$("[id=subjectsContainer]").setValue("Maths"); //Почему не работает? Элемент id=subjectsContainer единстенный на странице
+        //$(byText("Maths")).click();
 
-
-        //$("[id=subjectsContainer]").setValue("Maths"); //предметы
-        //$("[class=subjects-auto-complete__value-container--is-multi]").click();
-
-        //$(byText("Sports")).click();
-        //$("[id=uploadPicture]").setValue("some user"); // добавление файла, погуглить как отработать добавление файла
+        $(byText("Sports")).click();
         $("[id=currentAddress]").setValue("1600 Pennsylvania Avenue NW");
 
-        //$("[class=css-1uccc91-singleValue]", 2).click(); //штат
-        //$(byText("Select State")).click();
-        //$("[id=react-select-3-option-3]").click();
-        //$(byText("Rajasthan")).click();
+        /*
+        $("[class=css-1uccc91-singleValue]", 1).click(); //штат
+        $(byText("Haryana")).click();
+        Почему не работает? Индекс указан верно, по счету второй, счет начинается от нуля
+        */
 
-        //$(".css-1wa3eu0-placeholder", 3).click(); //город
-        //$(byText("Jaipur")).click();
+        $(byText("Select State")).click();
+        $(byText("Haryana")).click();
+
+        //$(".css-1wa3eu0-placeholder", 2).click(); //город
+        $(byText("Select City")).click();
+        $(byText("Panipat")).click();
+
         $(byText("Submit")).click();
-       // $x("//button[@id='submit']").click();
-
-        //$("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
 
 
+
+        $(byText("Student Name")).shouldHave(text("somefirstName somelastName"));
+        $(byText("Student Email")).shouldHave(text("tosttostovich@gmail.com"));
+        $(byText("Gender")).shouldHave(text("Male"));
+        $(byText("Mobile")).shouldHave(text("8999422555"));
+        $(byText("Date of Birth")).shouldHave(text("18 October,1989"));
+        $(byText("Hobbies")).shouldHave(text(""));
+        $(byText("Address")).shouldHave(text("1600 Pennsylvania Avenue NW"));
+        $(byText("State and City")).shouldHave(text("Haryana Submit"));
 
     }
 }
